32669c838d22b0e17374884d4d9a8e5b78933a19
Skokartong <amanda.olving@gmail.com>
2023-09-27 13:59:23 +0200

    Rolig historia.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Säg ett djur");
+string djur = Console.ReadLine();
+Console.WriteLine("Säg en färg");
+string färg = Console.ReadLine();
+Console.WriteLine("Välj en dryck");
+string dricka = Console.ReadLine();
+
+Console.WriteLine("Det var en gång en " + färg + " " + djur + "." + " Han drack " + dricka + " om dagarna.");
+Console.ReadLine();

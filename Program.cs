7b1eb0f44297fd8806b54efb716d823955d0be49
adrozs <adrozsahegyi@gmail.com>
2023-09-27 13:54:56 +0200

    added conditionals and Monika loop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+
+bool pull = true;
+bool push = true;
+
+for (int i = 0; i <= 15; i++)
+{
+    Console.WriteLine("Hej, hej Monika, hej på dig Monika!");
+}
+
+if (push == true)
+{
+    push = false;
+    pull = true;
+}
+
+if (pull = true)
+{
+    pull = false;
+    push = true;
+}
